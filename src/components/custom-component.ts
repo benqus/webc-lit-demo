5a3b205ce5be6205e1eaf275f67af14cfc880144
Bence Kormos <ben.kormos@gmail.com>
2022-03-28 14:52:50 +0100

    add css module example to components

--- src/components/custom-component.ts
+++ src/components/custom-component.ts
@@ -1,15 +1,22 @@
+import style from '../styles/element.module.css';
+
 export class CustomComponent extends HTMLElement {
   constructor() {
     super();
 
     // this.attachShadow({ mode: 'open' });
 
-    const p = document.createElement('p');
-    p.classList.add('text');
-    p.innerText = 'Custom WebComponent';
+    const p1 = document.createElement('p');
+    p1.classList.add('text');
+    p1.innerText = 'Custom WebComponent';
+    // this.shadowRoot.append(p1);
+    this.append(p1);
 
-    // this.shadowRoot.append(p);
-    this.append(p);
+    const p2 = document.createElement('p');
+    p2.classList.add(style.text);
+    p2.innerText = 'Custom WebComponent (CSS Modules)';
+    // this.shadowRoot.append(p2);
+    this.append(p2);
 
     const i = document.createElement('i');
     i.classList.add('fa-solid', 'fa-thumbs-up');
--- src/components/shadow-component.ts
+++ src/components/shadow-component.ts
@@ -1,18 +1,23 @@
+import style from '../styles/element.module.css';
+
 export class ShadowComponent extends HTMLElement {
   constructor() {
     super();
 
     this.attachShadow({ mode: 'open' });
 
-    const p = document.createElement('p');
-    p.classList.add('text');
-    p.innerText = 'Shadow WebComponent';
+    const p1 = document.createElement('p');
+    p1.classList.add('text');
+    p1.innerText = 'Shadow WebComponent';
+    this.shadowRoot.append(p1);
 
-    this.shadowRoot.append(p);
+    const p2 = document.createElement('p');
+    p2.classList.add(style.text);
+    p2.innerText = 'Shadow WebComponent (CSS Modules)';
+    this.shadowRoot.append(p2);
 
     const i = document.createElement('i');
     i.classList.add('fa-solid', 'fa-thumbs-up');
-
     this.shadowRoot.append(i);
   }
 }
